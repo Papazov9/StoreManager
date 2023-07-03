9c599f65f16de25c628b3b37ff7b42a4bf9045da
Papazov9 <blagovestpapazov987@gmail.com>
2023-07-03 21:31:41 +0300

    first commit

---
+++ src/main/java/com/returns/store/storagemanager/StorageManagerApplication.java
@@ -0,0 +1,13 @@
+package com.returns.store.storagemanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StorageManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StorageManagerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/returns/store/storagemanager/StorageManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.returns.store.storagemanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StorageManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
