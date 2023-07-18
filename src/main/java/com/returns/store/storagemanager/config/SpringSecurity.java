8886c16088edae1f9330adb0b3d130b2d06fd2a2
Papazov9 <blagovestpapazov987@gmail.com>
2023-07-18 20:14:54 +0300

    yana test

--- src/main/java/com/returns/store/storagemanager/config/SpringSecurity.java
+++ src/main/java/com/returns/store/storagemanager/config/SpringSecurity.java
@@ -6,37 +6,27 @@ import com.returns.store.storagemanager.service.ReturnsUserDetailsService;
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
+import static org.springframework.security.config.Customizer.withDefaults;
+
 @Configuration
+@EnableWebSecurity
 public class SpringSecurity {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests()
-                .requestMatchers(PathRequest.toStaticResources().atCommonLocations()).permitAll()
-                .requestMatchers("/", "/home", "/user/login", "/user/register", "/user/login-error").permitAll()
-                .requestMatchers("/static/**", "/js/**", "/css/**", "/img/**", "/videos/**").permitAll()
-//                .requestMatchers("/admin/**", "/api/users", "/products/edit/**").hasRole(RoleEnum.ADMIN.name())
-                .anyRequest().authenticated()
-                .and()
-                .formLogin()
-                .loginPage("/user/login")
-                .usernameParameter(UsernamePasswordAuthenticationFilter.SPRING_SECURITY_FORM_USERNAME_KEY)
-                .usernameParameter(UsernamePasswordAuthenticationFilter.SPRING_SECURITY_FORM_PASSWORD_KEY)
-                .defaultSuccessUrl("/", true)
-                .failureForwardUrl("/user/login-error")
-                .and()
-                .logout()
-                .logoutUrl("/user/logout")
-                .logoutSuccessUrl("/user/login")
-                .invalidateHttpSession(true)
-                .clearAuthentication(true)
-                .deleteCookies("JSESSIONID");
-
+        http.authorizeHttpRequests(authorize -> {
+            authorize.requestMatchers("/", "/home", "/user/login", "/user/register", "/user/login-error").permitAll(),
+            authorize.requestMatchers("/static/**", "/js/**", "/css/**", "/img/**", "/videos/**").permitAll(),
+            authorize.requestMatchers("/admin/**", "/api/users", "/products/edit/**").hasRole(RoleEnum.ADMIN.name()),
+            authorize.anyRequest().authenticated();
+        }).formLogin(withDefaults())
         return http.build();
 
     }
