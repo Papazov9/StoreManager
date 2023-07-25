3259d87b50e4ce332e43f75c2a0808905f36474c
Яна Христова Методиева <stu2201261016@uni-plovdiv.bg>
2023-07-25 16:04:21 +0300

    Unnecessary comments deleted from passwordToggler.js and import.html

--- src/main/resources/static/js/passwordToggler.js
+++ src/main/resources/static/js/passwordToggler.js
@@ -1,8 +1,6 @@
 const togglePassword = document.querySelector(".togglePassword");
 const password = document.querySelector("#password");
 
-console.log("I AM HERE")
-
 togglePassword.addEventListener("click", function () {
     const type = password.getAttribute("type") === "password" ? "text" : "password";
     password.setAttribute("type", type);
