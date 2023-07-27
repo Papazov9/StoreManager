c8518c2eb6bfe4f3c69b43179263805c656c2a61
Papazov9 <blagovestpapazov987@gmail.com>
2023-07-27 22:08:38 +0300

    Query specification MAYBE fixed!

--- src/main/java/com/returns/store/storagemanager/repo/ProductSpecification.java
+++ src/main/java/com/returns/store/storagemanager/repo/ProductSpecification.java
@@ -25,54 +25,54 @@ public class ProductSpecification implements Specification<SellingProduct> {
 
         if(productBinding.getReturnItemId() != null && !productBinding.getReturnItemId().trim().isEmpty()){
             p.getExpressions().add(
-                    cb.and(cb.equal(root.join("returnItemId").get("name"), productBinding.getReturnItemId()))
+                    cb.and(cb.equal(root.get("returnItemId"), productBinding.getReturnItemId()))
             );
         }
 
         if(productBinding.getAsin() != null && !productBinding.getAsin().trim().isEmpty()){
             p.getExpressions().add(
-                    cb.and(cb.equal(root.join("asin"), productBinding.getAsin())));
+                    cb.and(cb.equal(root.get("asin"), productBinding.getAsin())));
         }
 
         if(productBinding.getCategory() != null && !productBinding.getCategory().trim().isEmpty()){
             p.getExpressions().add(
                     cb
-                            .and(cb.equal(root.join("category").get("name"), productBinding.getCategory()))
+                            .and(cb.equal(root.get("category"), productBinding.getCategory()))
             );
         }
 
         if(productBinding.getCondition() != null && !productBinding.getCondition().trim().isEmpty()){
             p.getExpressions().add(
                     cb
-                            .and(cb.equal(root.join("condition"), productBinding.getCondition()))
+                            .and(cb.equal(root.get("condition"), productBinding.getCondition()))
             );
         }
 
         if(productBinding.getDepartment() != null && !productBinding.getDepartment().trim().isEmpty()){
             p.getExpressions().add(
                     cb
-                            .and(cb.equal(root.join("department"), productBinding.getDepartment()))
+                            .and(cb.equal(root.get("department"), productBinding.getDepartment()))
             );
         }
 
         if(productBinding.getEan() != null && !productBinding.getEan().trim().isEmpty()){
             p.getExpressions().add(
                     cb
-                            .and(cb.equal(root.join("ean"), productBinding.getEan()))
+                            .and(cb.equal(root.get("ean"), productBinding.getEan()))
             );
         }
 
         if(productBinding.getLpn() != null && !productBinding.getLpn().trim().isEmpty()){
             p.getExpressions().add(
                     cb
-                            .and(cb.equal(root.join("lpn"), productBinding.getLpn()))
+                            .and(cb.equal(root.get("lpn"), productBinding.getLpn()))
             );
         }
 
         if(productBinding.getPalletId() != null && !productBinding.getPalletId().trim().isEmpty()){
             p.getExpressions().add(
                     cb
-                            .and(cb.equal(root.join("palletId"), productBinding.getPalletId()))
+                            .and(cb.equal(root.get("palletId"), productBinding.getPalletId()))
             );
         }
 
