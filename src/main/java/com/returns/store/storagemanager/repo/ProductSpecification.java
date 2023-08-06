7c040eecc331e0bd903701b79170a2cbc5a5fbb1
Яна Христова Методиева <stu2201261016@uni-plovdiv.bg>
2023-08-06 18:01:08 +0300

    Sorry

--- src/main/java/com/returns/store/storagemanager/repo/ProductSpecification.java
+++ src/main/java/com/returns/store/storagemanager/repo/ProductSpecification.java
@@ -2,11 +2,12 @@ package com.returns.store.storagemanager.repo;
 
 import com.returns.store.storagemanager.model.bindings.SearchProductBinding;
 import com.returns.store.storagemanager.model.entity.SellingProduct;
+import org.springframework.data.jpa.domain.Specification;
+
 import jakarta.persistence.criteria.CriteriaBuilder;
 import jakarta.persistence.criteria.CriteriaQuery;
 import jakarta.persistence.criteria.Predicate;
 import jakarta.persistence.criteria.Root;
-import org.springframework.data.jpa.domain.Specification;
 
 public class ProductSpecification implements Specification<SellingProduct> {
 
@@ -24,56 +25,35 @@ public class ProductSpecification implements Specification<SellingProduct> {
         Predicate p = cb.conjunction();
 
         if(productBinding.getReturnItemId() != null && !productBinding.getReturnItemId().trim().isEmpty()){
-            p.getExpressions().add(
-                    cb.and(cb.equal(root.get("returnItemId"), productBinding.getReturnItemId()))
-            );
+            p = cb.and(p, cb.equal(root.get("returnItemId"), productBinding.getReturnItemId()));
         }
 
         if(productBinding.getAsin() != null && !productBinding.getAsin().trim().isEmpty()){
-            p.getExpressions().add(
-                    cb.and(cb.equal(root.get("asin"), productBinding.getAsin())));
+            p = cb.and(p, cb.equal(root.get("asin"), productBinding.getAsin()));
         }
 
         if(productBinding.getCategory() != null && !productBinding.getCategory().trim().isEmpty()){
-            p.getExpressions().add(
-                    cb
-                            .and(cb.equal(root.get("category"), productBinding.getCategory()))
-            );
+            p = cb.and(p, cb.equal(root.get("category"), productBinding.getCategory()));
         }
 
         if(productBinding.getCondition() != null && !productBinding.getCondition().trim().isEmpty()){
-            p.getExpressions().add(
-                    cb
-                            .and(cb.equal(root.get("condition"), productBinding.getCondition()))
-            );
+            p = cb.and(p, cb.equal(root.get("condition"), productBinding.getCondition()));
         }
 
         if(productBinding.getDepartment() != null && !productBinding.getDepartment().trim().isEmpty()){
-            p.getExpressions().add(
-                    cb
-                            .and(cb.equal(root.get("department"), productBinding.getDepartment()))
-            );
+            p = cb.and(p, cb.equal(root.get("department"), productBinding.getDepartment()));
         }
 
         if(productBinding.getEan() != null && !productBinding.getEan().trim().isEmpty()){
-            p.getExpressions().add(
-                    cb
-                            .and(cb.equal(root.get("ean"), productBinding.getEan()))
-            );
+            p = cb.and(p, cb.equal(root.get("ean"), productBinding.getEan()));
         }
 
         if(productBinding.getLpn() != null && !productBinding.getLpn().trim().isEmpty()){
-            p.getExpressions().add(
-                    cb
-                            .and(cb.equal(root.get("lpn"), productBinding.getLpn()))
-            );
+            p = cb.and(p, cb.like(root.get("lpn"), productBinding.getLpn()));
         }
 
         if(productBinding.getPalletId() != null && !productBinding.getPalletId().trim().isEmpty()){
-            p.getExpressions().add(
-                    cb
-                            .and(cb.equal(root.get("palletId"), productBinding.getPalletId()))
-            );
+            p = cb.and(p, cb.equal(root.get("palletId"), productBinding.getPalletId()));
         }
 
         return p;
